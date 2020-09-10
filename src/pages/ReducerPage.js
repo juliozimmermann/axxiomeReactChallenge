b3bb7fc12474f24f778a3d1bab1ff2812a3eeac9
Julio Zimmermann <juliozimmermann@gmail.com>
2020-09-10 11:23:26 -0300

    Internal Challenge

--- src/pages/ReducerPage.js
+++ src/pages/ReducerPage.js
@@ -1,34 +1,32 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useState, useCallback, useReducer, useMemo } from 'react'
 
-const SET_STEP = 'SET_STEP';
-const SET_LIMIT_CONTROL = 'SET_LIMIT_CONTROL';
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const PREVENT_NEGATIVE = 'PREVENT_NEGATIVE';
 
-const counterReducer = (state, { type, payload }) => {
+function counterReducer(state, { type, payload }) {
   switch (type) {
-    case SET_STEP:
+    case INCREASE:
       return {
         ...state,
-        step: payload
+        counter: state.counter + 1
       }
-    case SET_LIMIT_CONTROL:
+    case DECREASE: 
+      let newCounter = 0;
+
+      if (state.preventNegativeCounter && state.counter <= 0)
+        newCounter = state.counter;
+      else 
+        newCounter = state.counter - 1;
+
       return {
         ...state,
-        preventNegativeCounter: payload
+        counter: newCounter
       }
-    case INCREMENT: 
+    case PREVENT_NEGATIVE:
       return {
         ...state,
-        counter: state.counter + state.step
-      }
-    case DECREMENT: 
-      const { counter, step, preventNegativeCounter } = state;
-      const newCounter = counter - step;
-      const updateCounter = !preventNegativeCounter || (preventNegativeCounter && newCounter >= 0);
-      return {
-        ...state,
-        counter: updateCounter ? newCounter : counter,
+        preventNegativeCounter: !state.preventNegativeCounter
       }
     default:
       return state;
@@ -36,36 +34,25 @@ const counterReducer = (state, { type, payload }) => {
 }
 
 const initialState = () => ({
-  step: 1,
   counter: 0,
   preventNegativeCounter: false
-})
+});
 
-function ReducerPage() {
-  const [{ step, counter, preventNegativeCounter }, dispatch] = useReducer(counterReducer, initialState());
+export default function CounterState() {
+  const [{ counter, preventNegativeCounter }, dispatch] = useReducer(counterReducer, initialState());
 
-  const handleStepChange = useCallback(
-    ({ target: { value }}) => dispatch({ type: SET_STEP, payload: parseInt(value)})
-  , []);
-  const handlePreventNegativeChange = useCallback(
-    ({ target: { checked }}) => dispatch({ type: SET_LIMIT_CONTROL, payload: checked})
-  , []);
-  const handleIncrement = useCallback(() => dispatch({ type: INCREMENT }), []);
-  const handleDecrement = useCallback(() => dispatch({ type: DECREMENT }), []);
+  const handlePreventNegativeCounter = useCallback(() => dispatch({ type: PREVENT_NEGATIVE }), []);
+
+  const handleIncreaseClick = useCallback(() => dispatch({ type: INCREASE }), []);
+  const handleDecreaseClick = useCallback(() => dispatch({ type: DECREASE }), []);
 
   return <>
-    <h2>Reducer Page</h2>
-    <fieldset>
-      <legend>Setup Counter</legend>
-      <label>Counter step</label><br />
-      <input type="number" value={step} onChange={handleStepChange} /><br />
-      <input type="checkbox" checked={preventNegativeCounter}  onChange={handlePreventNegativeChange} /><label>Prevent negative counter</label><br />
-    </fieldset>
-
-    <h3>{counter}</h3>
-    <button onClick={handleIncrement}>Increment</button>
-    <button onClick={handleDecrement}>Decrement</button>
+    <h1>Counter Reducer</h1>
+    <input type="checkbox" selected={preventNegativeCounter} onChange={handlePreventNegativeCounter} />
+    <label>Prevent negative counter</label>
+    <br />
+    {counter}
+    <button type="button" onClick={handleIncreaseClick}>Increase</button>
+    <button type="button" onClick={handleDecreaseClick}>Decrease</button>
   </>;
 }
-
-export default ReducerPage;
